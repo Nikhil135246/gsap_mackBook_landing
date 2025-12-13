c8dc645bbf7ad04b8e92c47e48030b3408df067e
Nikhil <nikhilkurrey135@gmail.com>
2025-12-13 22:38:02 +0530

    vite.config.js change added oallowedhosts line

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,7 @@ export default defineConfig({
   // step 2.2 was added tailwindcss(), in the plugins array
   plugins: [react(),tailwindcss()],
   // step 2.3 to import tailwindcss in index.css
+  preview:{
+    allowedHosts:['.onrender.com']
+  }
 })
